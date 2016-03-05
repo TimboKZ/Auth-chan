5cfd5eeb36f3cbb7c42da4c83bc419811c734e19
TimboKZ <tima_kuzh@inbox.ru>
2016-03-05 10:38:03 +0000

    Added CSS autoprefixer and minificator, updated the demo page

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 gulp.task('sass', function () {
     return gulp.src('src/sass/auth-chan.sass')
         .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(cleanCSS())
+        .pipe(rename('auth-chan.min.css'))
         .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.reload({
             stream: true
@@ -29,7 +36,7 @@ gulp.task('browserSync', function () {
     })
 });
 
-gulp.task('default', ['browserSync', 'sass'], function () {
+gulp.task('default', ['browserSync', 'sass', 'ts'], function () {
     gulp.watch('src/sass/auth-chan.sass', ['sass']);
     gulp.watch('src/ts/auth-chan.ts', ['ts']);
     gulp.watch('index.html', browserSync.reload);
